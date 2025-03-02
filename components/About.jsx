b8b2d055b8d33ad55d8fa7889a2014e05c3ee097
mashtheking <fredmaina174@gmail.com>
2025-03-02 23:38:57 +0300

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -11,14 +11,9 @@ const About = () => {
           </div>
         </div>
         <div className="md:w-1/2 ">
-          <Title addClass="text-[40px]">We Are Feane</Title>
+          <Title addClass="text-[40px]">We Are Authentic African cuisine</Title>
           <p className="my-5 flex flex-col items-center">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don`t look even slightly
-            believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn`t anything embarrassing hidden in the
-            middle of text. All
+            We provide Africans abroad with Authentic African cuisine.
           </p>
           <button className="btn-primary">Read More</button>
         </div>
