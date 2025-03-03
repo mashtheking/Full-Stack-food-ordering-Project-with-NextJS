c0189aed3f73859ac8a9da6bd1eea9e1dc026d4d
mashtheking <fredmaina174@gmail.com>
2025-03-03 17:31:50 +0300

    Update mongo.js

--- util/mongo.js
+++ util/mongo.js
@@ -11,9 +11,7 @@ const options = {
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
+
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
