aae729beb45aeb3caffd674527fff931aa9d9283
mashtheking <fredmaina174@gmail.com>
2025-03-03 20:34:48 +0300

    Update [id].jsx

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -89,7 +89,7 @@ const Profile = ({ user }) => {
 
 export async function getServerSideProps({ req, params }) {
   const user = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/users/${params.id}`
+    `https://full-stack-food-ordering-project-with-next-js-six.vercel.app/users/${params.id}`
   );
 
   return {
