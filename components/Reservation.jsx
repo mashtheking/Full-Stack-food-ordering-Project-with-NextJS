df67efa422d824e2114a7317234454a082ce5dec
mashtheking <fredmaina174@gmail.com>
2025-03-04 11:42:11 +0300

    Update Reservation.jsx

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -92,14 +92,9 @@ const Reservation = () => {
           </button>
         </form>
         <div className="lg:flex-1 w-full">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48348.66924008447!2d-74.24927437205034!3d40.766603131286395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c254b5958982c3%3A0xb6ab3931055a2612!2sEast%20Orange%2C%20New%20Jersey%2C%20Amerika%20Birle%C5%9Fik%20Devletleri!5e0!3m2!1str!2str!4v1661853137161!5m2!1str!2str"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="h-full w-full"
-          ></iframe>
-        </div>
+
+  <iframe width="81%" height="650px" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB2NIWI3Tv9iDPrlnowr_0ZqZWoAQydKJU&q=Gaithersburg%2C%20MD%2C%20USA&maptype=roadmap" allowfullscreen></iframe>
+
       </div>
     </div>
   );
