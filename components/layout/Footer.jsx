7978733ea0a11d4fdd0574db55f65dc3b6963cb8
mashtheking <fredmaina174@gmail.com>
2025-03-04 12:29:00 +0300

    Update Footer.jsx

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
                 <i className="fa fa-phone"></i>
                 <a
                   className="inline-block ml-2"
-                  href={`tel:${footer?.phoneNumber}`}
+                  href={`tel:+19087977164`}
                 >
-                  {footer?.phoneNumber}
+                +19087977164
                 </a>
               </div>
               <a href={`mailto:${footer?.email}`}>
@@ -44,7 +44,7 @@ const Footer = () => {
             </div>
           </div>
           <div className="md:flex-1">
-            <Title addClass="text-[38px]">Feane</Title>
+            <Title addClass="text-[38px]"></Title>
             <p className="mt-3">{footer?.desc}</p>
             <div className="flex items-center justify-center mt-5 gap-x-2">
               {footer?.socialMedia?.map((item, index) => (
@@ -64,13 +64,13 @@ const Footer = () => {
             <Title addClass="text-[30px]">Opening Hours</Title>
             <div className="flex flex-col gap-y-2 mt-3">
               <div>
-                <span className="inline-block ml-2">
-                  {footer?.openingHours?.day}
+                <span className="inline-block ml-2">Sunday - Friday
                 </span>
               </div>
               <div>
                 <span className="inline-block ml-2">
-                  {footer?.openingHours?.hour}
+                  
+                  8 a.m. to 9 p.m.
                 </span>
               </div>
             </div>
