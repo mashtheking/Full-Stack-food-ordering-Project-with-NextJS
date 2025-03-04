69228535643be5af74d4db20840dd0766a31d46c
mashtheking <fredmaina174@gmail.com>
2025-03-04 04:47:04 +0300

    Update [id].jsx

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -9,6 +9,13 @@ const Order = ({ order }) => {
     if (index - status === 1) return "animate-pulse";
     if (index - status > 1) return "";
   };
+
+const initialOptions = {
+  "client-id": "AZ9wjyzRaq1x4UehdhLkj2EJEtOYoxUM20xyATjaOAm_LRkEjSw0Ddj9IAbRQOmVjLoBTaybaQgpSqHB",
+  currency: "USD",
+  intent: "capture",
+};
+
   return (
     <div className="overflow-x-auto">
       <div className="min-h-[calc(100vh_-_433px)] flex  justify-center items-center flex-col p-10  min-w-[1000px]">
